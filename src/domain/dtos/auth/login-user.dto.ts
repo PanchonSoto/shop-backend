be5cccf634811ae06512a09a2cd871665c4e4285
Francisco Soto <panchon.ues@gmail.com>
2025-02-06 10:51:45 -0700

    remove login password regex

--- src/domain/dtos/auth/login-user.dto.ts
+++ src/domain/dtos/auth/login-user.dto.ts
@@ -1,32 +1,24 @@
 import { RegexUtils } from "../../../config/regex-utils";
 
-
 export class LoginUserDto {
+  private constructor(
+    public email: string,
+    public password: string
+  ) {}
 
-    private constructor(
-        public email: string,
-        public password: string,
-    ){}
+  static create(object: { [key: string]: any }): [string?, LoginUserDto?] {
+    const { email, password } = object;
 
+    if (!email) return ["email is required"];
+    if (!RegexUtils.email.test(email)) return ["email is invalid"];
 
+    if (!password) return ["password is required"];
+    // if(!RegexUtils.password.test(password)) return ['password must have at least one number, one lowercase and one uppercase letter, and between 6 and 20 characters'];
 
-    static create(object:{[key: string]: any}): [string?, LoginUserDto?] {
-        const { email, password } = object;
+    return [undefined, new LoginUserDto(email, password)];
+  }
 
-
-        if(!email) return ['email is required'];
-        if(!RegexUtils.email.test(email)) return ['email is invalid'];
-
-        if(!password) return ['password is required'];
-        if(!RegexUtils.password.test(password)) return ['password must have at least one number, one lowercase and one uppercase letter, and between 6 and 20 characters'];
-
-        return [undefined, new LoginUserDto(email, password)];
-
-    }
-
-
-    // static create(name: string, email: string, password: string, role: string): RegisterUserDto {
-    //     return new RegisterUserDto(name, email, password, role);
-    // }
-
+  // static create(name: string, email: string, password: string, role: string): RegisterUserDto {
+  //     return new RegisterUserDto(name, email, password, role);
+  // }
 }
